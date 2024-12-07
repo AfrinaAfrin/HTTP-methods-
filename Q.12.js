03d26d24d9a9e40024df298dd667ae4fda937930
AfrinaAfrin <afrinaafrin1033@gmail.com>
2024-12-07 13:22:14 +0530

    added new file

---
+++ Q.12.js
@@ -0,0 +1,10 @@
+ 
+const chalk = require('chalk');
+
+ 
+console.log(chalk.blue.bold('Hello, Node.js!'));
+
+ 
+console.log(chalk.yellow('Caution: Low Disk Space'));
+ 
+console.log(chalk.green('Installation Complete!'));
