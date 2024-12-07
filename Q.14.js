9e9b630c44640bd9437c4b73b3826613703ad26c
AfrinaAfrin <afrinaafrin1033@gmail.com>
2024-12-07 13:27:38 +0530

    added new file

---
+++ Q.14.js
@@ -0,0 +1,15 @@
+ 
+
+let time = 5;
+
+ 
+const intervalId = setInterval(() => {
+  if (time >= 0) {
+    console.log(`Time left: ${time} seconds`);
+    time--;
+  } else {
+    console.log('Countdown finished!');
+    clearInterval(intervalId);  
+  }
+}, 1000);
+ 
